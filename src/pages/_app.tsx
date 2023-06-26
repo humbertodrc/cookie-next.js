13bfadbdc48ca8b30793c5e0765c0e4a07d3f734
Humberto Rivero <humbertorivero9@gmail.com>
2023-06-26 16:51:48 -0300

    Add Material UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import {darkTheme, customTheme, lightTheme} from "@/theme";
+import {CssBaseline, ThemeProvider} from "@mui/material";
+import type {AppProps} from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}: AppProps) {
+	return (
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
 }
