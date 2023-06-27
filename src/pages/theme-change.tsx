4cd1c7e47748015fcbb7b06986e8698be195c616
Humberto Rivero <humbertorivero9@gmail.com>
2023-06-27 16:35:31 -0300

    Feat send and read cookies theme

--- src/pages/theme-change.tsx
+++ src/pages/theme-change.tsx
@@ -6,15 +6,30 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import FormLabel from '@mui/material/FormLabel';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
-import React from "react";
+import React, { FC, useEffect } from "react";
+import { GetServerSideProps } from 'next';
+import Cookies from 'js-cookie';
 
-const ThemeChangePage = () => {
+const ThemeChangePage: FC = (props) => {
+  
+  console.log(props);
 
   const [currentTheme, setCurrentTheme] = React.useState('dark')
 
+  /**
+   * Funcion que se ejecuta cuando se cambia el tema
+   * @param event 
+   */
   const handleChangeTheme = (event: React.ChangeEvent<HTMLInputElement>) => {
     setCurrentTheme(event.target.value)
+
+    localStorage.setItem('theme', event.target.value)
+    Cookies.set('theme', event.target.value)
   }
+
+  useEffect(() => {
+    console.log(localStorage.getItem('theme'))
+  }, [])
 
 	return (
 		<Layout>
@@ -29,7 +44,7 @@ const ThemeChangePage = () => {
               onChange={handleChangeTheme}
             >
               <FormControlLabel value='light' control={<Radio />} label='light' />
-              <FormControlLabel value='dark' control={<Radio />} label='dark' />
+              <FormControlLabel value='dark' control={<Radio />} label='dark'/>
               <FormControlLabel value='custom' control={<Radio />} label='custom' />
             </RadioGroup>
           </FormControl>
@@ -38,5 +53,20 @@ const ThemeChangePage = () => {
 		</Layout>
 	);
 };
+
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  
+  const {theme = 'dark'} = req.cookies;
+
+  return {
+    props: {
+      theme
+    }
+  }
+}
 
 export default ThemeChangePage;
