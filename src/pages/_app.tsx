f9e42662a867c853fced4305a40e514cedb0bc9d
Humberto Rivero <humbertorivero9@gmail.com>
2023-07-12 10:49:35 -0300

    Feat cookie management in App

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,30 @@
 import "@/styles/globals.css";
 import {darkTheme, customTheme, lightTheme} from "@/theme";
 import {CssBaseline, ThemeProvider, Theme} from "@mui/material";
+import Cookies from 'js-cookie';
 import type {AppContext, AppProps} from "next/app";
+import { useEffect, useState } from 'react';
 
 interface Props extends AppProps {
 	theme: string
 }
 
-export default function App({ Component, pageProps, theme }: Props) {
-	
-	const currentTheme:Theme = theme === 'light' ? lightTheme : theme === 'dark' ? darkTheme : customTheme;
+export default function App({ Component, pageProps, theme = 'dark' }: Props) {
+
+	const [currentTheme, setCurrentTheme] = useState(lightTheme)
+
+	useEffect(() => {
+		
+		const cookieTheme = Cookies.get('theme') || 'light';
+		const selectedTheme: Theme = cookieTheme === 'light'
+		? lightTheme
+		: cookieTheme === 'dark'
+			? darkTheme
+			: customTheme;
+		
+		setCurrentTheme(selectedTheme)
+
+	}, [])
 	
 
 	return (
@@ -20,19 +35,19 @@ export default function App({ Component, pageProps, theme }: Props) {
 	);
 }
 
-App.getInitialProps = async (ctx: AppContext) => {
+// App.getInitialProps = async (ctx: AppContext) => {
 
-	// Obtener la cookie "theme"
-  const cookie = ctx.ctx.req?.headers.cookie;
-  const themeCookie = cookie?.split(';').find((c) => c.trim().startsWith('theme='));
+// 	// Obtener la cookie "theme"
+//   const cookie = ctx.ctx.req?.headers.cookie;
+//   const themeCookie = cookie?.split(';').find((c) => c.trim().startsWith('theme='));
 
-  // Obtener el valor del tema de la cookie o usar "dark" como valor predeterminado
-  const theme = themeCookie ? themeCookie.split('=')[1] : 'dark';
+//   // Obtener el valor del tema de la cookie o usar "dark" como valor predeterminado
+//   const theme = themeCookie ? themeCookie.split('=')[1] : 'dark';
 	
-	  // Validar el theme
-		const validThemes = ['light', 'dark', 'custom']
+// 	  // Validar el theme
+// 		const validThemes = ['light', 'dark', 'custom']
 
-	return {
-		theme: validThemes.includes(theme) ? theme : 'dark'
-	}
-};	
+// 	return {
+// 		theme: validThemes.includes(theme) ? theme : 'dark'
+// 	}
+// };	
